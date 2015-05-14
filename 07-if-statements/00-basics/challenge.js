5c1878ad694125b18e7159e7134551c995074349
rmcgarity <ralph_mcgarity@yahoo.com>
2015-05-14 12:53:18 +0000

    Reran with the correct mocha command, and fixed two errors. All mocha tests pass now.

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -23,7 +23,7 @@ module.exports.checkLock = function(first, second, third, fourth) {
     if ((first == 3 || first == 5 || first == 7) && 
       (second == 2) &&
       (third >= 5 && third <= 100) && 
-      (fourth <= 9 || fourth >= 20)) {
+      (fourth < 9 || fourth > 20)) {
         return "correct";
     } else {
         return "incorrect";
@@ -40,7 +40,7 @@ module.exports.canIGet = function(item, money) {
     
     // Jean-Denis, let me know if you want this coded with if-then-else. Below is simpler, though.
     return (item == "MacBook Air" && money >= 999) ||
-      (item == "MacBook Pro" && money >=1200) ||
+      (item == "MacBook Pro" && money >=1299) ||
       (item == "Mac Pro" && money >=2499) ||
       (item == "Apple Sticker" && money >= 1);
 };
