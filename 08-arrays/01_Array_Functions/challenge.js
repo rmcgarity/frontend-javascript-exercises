1ee5b5a31a2767c2133d987d0749f2a3dc1f6526
rmcgarity <ralph_mcgarity@yahoo.com>
2015-05-14 21:54:23 +0000

    Tweaked the solution for sub-exercise 01 - replaced for loop with toString method.

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -13,11 +13,8 @@ module.exports.plusesEverywhere = function(inArray) {
     // This function should return:
     // a String made of all the values in the array separated by `+`
 
-    var returnString = "";
-    for (var i = 0;  i < inArray.length-1; i++) {
-        returnString += inArray[i] + "+";
-    }
-    returnString += inArray[inArray.length-1];
+    var returnString = inArray.toString();
+    var returnString = returnString.replace(/,/g, "+")
     return returnString;
 };
 
