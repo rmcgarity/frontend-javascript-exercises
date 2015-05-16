f24a8f6ce562d8fb8a87a9961a51603568825644
rmcgarity <ralph_mcgarity@yahoo.com>
2015-05-16 13:54:04 +0000

    Completed exercise-10

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,29 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(inputArray) {
+    // Return the sum of all numbers inside of the array that is passed to the function
+    // If the array is empty, this function should return 0
+    var result = 0;
+    for (var i=0; i < inputArray.length; i++) {
+        result += inputArray[i];
+    }
+    return result;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inString) {
+    // - split the `inputString` argument into an array of strings by commas
+    // - return an array with the set of split strings in lowercase format
+    
+    // It's not clear to me how this exercise is related to looping.
+    // Maybe it's a review exercise?
+    
+    return(inString.toLocaleLowerCase().split(","));
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(inObject) {
+     // EX: addIndex(['My', 1, 'number']) // returns ["0 is My", "1 is 1", "2 is number"]
+     var returnArray = [];
+     var i = 0;
+     for (var item in inObject) {
+         returnArray[i++] = item + " is " + inObject[item];
+     }
+     return(returnArray);
+};
